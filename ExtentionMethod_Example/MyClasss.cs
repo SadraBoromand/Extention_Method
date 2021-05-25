19a9d8cfebb23046ab77859bbaadb52632695908
SadraBoromand <sadrabroo@gmail.com>
2021-05-25 10:39:34 +0430

    main project

---
+++ ExtentionMethod_Example/MyClasss.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExtentionMethod_Example
+{
+    public static class MyClasss
+    {
+        public static string ToRial(this int value)
+        {
+            return value.ToString("#,0 Tooman");
+        }
+
+        public static string ToShamsi(this DateTime value)
+        {
+            PersianCalendar persianCalendar=new PersianCalendar();
+            return $"{persianCalendar.GetYear(value)}/{persianCalendar.GetMonth(value).ToString("00")}/{persianCalendar.GetDayOfMonth(value).ToString("00")}";
+        }
+    }
+}
---
+++ ExtentionMethod_Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethod_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Amount = 25000;
+            //Console.WriteLine(Amount.ToString("#,0 Rial"));
+            //Console.WriteLine(MyClasss.ToRial(Amount));
+            //Console.WriteLine(MyClasss.ToRial(741258));
+
+            Console.WriteLine(13132.ToRial()); 
+            Console.WriteLine(DateTime.Now.ToShamsi());
+            Console.ReadKey();
+        }
+    }
+}
